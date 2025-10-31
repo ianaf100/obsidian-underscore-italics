e12146655b06b92b4797627c61339d88481c0d06
Ian Franklin <ianaf100@gmail.com>
2025-10-31 16:30:16 -0600

    Working-ish prototype

--- main.ts
+++ main.ts
@@ -1,12 +1,15 @@
 // import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
-import { App, Editor, MarkdownView, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, EditorRange, EditorSelection as EditorSelectionOb, MarkdownView, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { EditorView } from "@codemirror/view";
+import { ChangeSpec, EditorSelection as EditorSelectionCM, EditorState, SelectionRange } from "@codemirror/state";
 
 // Remember to rename these classes and interfaces!
 
-type ItalicCharChoice = 'underscore' | 'asterisk';
+type DelimiterCharacter = '_' | '*';
+// type ItalicCharChoice = 'underscore' | 'asterisk';
 
 interface UnderscoreItalicsSettings {
-	defaultItalic: ItalicCharChoice;
+	defaultItalic: 'underscore' | 'asterisk';
     autoConvert: boolean;
 }
 
@@ -14,11 +17,16 @@ const DEFAULT_SETTINGS: Partial<UnderscoreItalicsSettings> = {
 	defaultItalic: 'underscore',
 }
 
+const IS_ITALICIZED = /_[a-zA-z0-9]*_|_\*\*[a-zA-z0-9]*\*\*_/;
+
 export default class UnderscoreItalics extends Plugin {
 	settings: UnderscoreItalicsSettings;
+	delim: DelimiterCharacter;
 
 	async onload() {
 		await this.loadSettings();
+
+		if (this.settings.defaultItalic === 'asterisk') 
 		console.log('loading obsidian-underscore-italics plugin');
 
 		// This adds an editor command that can perform some operation on the current editor instance
@@ -34,72 +42,25 @@ export default class UnderscoreItalics extends Plugin {
 		this.addCommand({
 			id: 'toggle-italic',
 			name: 'Toggle italic',	
-			editorCheckCallback: (checking: boolean, editor: Editor) => {
+			editorCheckCallback: (checking: boolean, editor: Editor, view: MarkdownView) => {
 				// Conditions to check
 				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
 				if (markdownView) {
+					// @ts-expect-error, not typed
+					const editorView = view.editor.cm as EditorView;
+
 					if (!checking) {
 						// If checking is false, we actually perform the operation:
-
-						if (!editor.somethingSelected()) {
-							let wordSelection = editor.wordAt(editor.getCursor());
-							if (wordSelection == null) {
-								// Empty selection (simple case)
-								console.log("Empty selection");
-								editor.replaceSelection('_');
-								let pos = editor.getCursor();
-								editor.replaceSelection('_');
-								editor.setCursor(pos);
-							} else {
-								// At the cursor, italicize the surrounding word
-								let word = editor.getRange(wordSelection.from, wordSelection.to);
-								let italicized = '_' + word + '_'
-								let pos = editor.getCursor();
-								editor.replaceRange(italicized, wordSelection.from, wordSelection.to);
-								pos.ch = pos.ch + 1;
-								editor.setCursor(pos);
-							}
-						} else {
-							if (editor.listSelections().length == 1) {
-								let selected = editor.getSelection();
-								console.log('Selection(1): ' + selected); // refactor into function
-								let italicized = '_' + selected + '_';
-								editor.replaceSelection(italicized);
-							} else {
-								let selections = editor.listSelections();
-								selections.forEach((elem, i) => {
-									let anchorpos = elem.anchor.line + elem.anchor.ch;
-									let headpos = elem.head.line + elem.head.ch;
-									let from = anchorpos < headpos ? elem.anchor : elem.head;
-									let to = anchorpos < headpos ? elem.head : elem.anchor;
-									let selected = editor.getRange(from, to);  // refactor into function
-									// TODO: trim/update selection
-									console.log(`Selection(${i+1}): ${selected}`);
-									let italicized = '_' + selected + '_';
-									editor.replaceRange(italicized, from, to);
-								});
-							}
-						}
-
+						this.toggleItalics(editor, editorView);
 					}
 					// If checking is true, we're only checking if the command 'can' be run.
-					// This command will only show up in Command Palette when the check function returns true.
+					// This command will only show up in Command Palette when this check function returns true.
 					// Here we're just checking that MarkdownView is active.
 					return true;
 				}
 			}
 		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new MySettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		// this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-		// 	console.log('click', evt);
-		// });
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		// this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
@@ -108,11 +69,136 @@ export default class UnderscoreItalics extends Plugin {
 
 	async loadSettings() {
 		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		this.delim = (this.settings.defaultItalic === 'asterisk') ? '*' : '_';
 	}
 
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
+
+	toggleItalics(editor: Editor, view: EditorView) {
+		let { state } = view;
+		let editorSelected = state.selection;
+		
+		let selectionRanges: SelectionRange[] = [];
+		let changeList: ChangeSpec[] = [];
+
+		// 1. Single cursor/selection
+		if (editorSelected.ranges.length == 1) {
+			let range = editorSelected.main;
+			const transaction = this.generateTransaction(state, range);
+			selectionRanges.concat(transaction.selectionRanges);
+			changeList.concat(transaction.changes);
+
+		// 2. If there's multiple cursors/selections
+		} else {
+			let selections = editorSelected.ranges;
+			let removeItalics = false;  // ? 
+
+			selections.forEach((selection) => {
+				let which: boolean = this.checkWordForItalic(state, selection);  // ?
+				const partialTransaction = this.generateTransaction(state, selection);
+				changeList.concat(partialTransaction.changes);
+				selectionRanges.concat(partialTransaction.selectionRanges);
+			});
+			
+			// Execute the given list of update transactions in the editor view
+			view.dispatch({
+				changes: changeList,
+				selection: EditorSelectionCM.create(selectionRanges)
+			});
+
+			//// Note: Obsidian default behavior seems to be if there are multiple selections, then it becomes a 
+			////   UNIVERAL operation to either italicize OR unitalicize for EVERY candidate. However I can't
+			////   figure out how it determines *which* it decides to pick if there's a mixture.
+		}
+	}
+
+	// TODO? add spaces to the regex spec to make it more strict, bc false positives are possible 
+
+	generateTransaction(state: EditorState, range: SelectionRange) {
+		let cursorPos = undefined;
+		
+		// Cursor only, no selection
+		if (range.empty) {
+			// Attempt to expand the selection to the surrounding word at the cursor point 
+			cursorPos = range.anchor;
+			let selectedWord = state.wordAt(range.anchor);
+			range = EditorSelectionCM.range(selectedWord?.from ?? cursorPos, selectedWord?.to ?? cursorPos);
+		}
+
+		// Checks if the immediate selection is italicized already
+		if (this.checkWordForItalic(state, range)) {
+			return this.unitalicizeTransaction(state, range, cursorPos);
+		} else {
+			return this.italicizeTransaction(range, cursorPos);
+		}
+	}
+
+	unitalicizeTransaction(state: EditorState, range: SelectionRange, cursorPos?: number) {
+		let cursorUpdates: SelectionRange[];
+		if (cursorPos) {
+			cursorUpdates = [EditorSelectionCM.cursor(Math.max(0, cursorPos-1))];  // moves cursor back 1
+		} else { 
+			cursorUpdates = [EditorSelectionCM.range(range.anchor-1, range.head-1)];
+		}
+		let updatedRange = this.updateRange(state, range);
+		return {
+			selectionRanges: cursorUpdates,
+			changes: [
+				{ from: updatedRange.to, to: (updatedRange.to + 1), insert: '' },
+				{ from: updatedRange.from, to: (updatedRange.from + 1), insert: '' }
+			]
+		};
+	}
+
+	italicizeTransaction(range: SelectionRange, cursorPos?: number) {
+		let cursorUpdates: SelectionRange[];
+		if (cursorPos) 
+			cursorUpdates = [EditorSelectionCM.cursor(cursorPos+1)]; // move cursor forward 1
+		else 
+			cursorUpdates = [EditorSelectionCM.range(range.anchor+1, range.head+1)];
+		return {
+			selectionRanges: cursorUpdates,
+			changes: [
+				{ from: range.from, insert: `${this.delim}` },
+				{ from: range.to,   insert: `${this.delim}` }
+			]
+		};
+	}
+
+	// Lazy check to see if current word has underscores around it
+	checkWordForItalic(state: EditorState, range: SelectionRange, radius: number = 3) {
+		radius = Math.min(radius, range.from);
+		const token = state.sliceDoc(range.from - radius, range.to + radius);
+		return token.match(IS_ITALICIZED) != null;
+	}
+
+	// Return a new selection that's expanded to include nearby enclosing italics
+	updateRange(state: EditorState, range: SelectionRange, radius: number = 3) {
+		radius = Math.min(radius, range.from);
+		const token = state.sliceDoc(range.from - radius, range.to + radius);
+		const match = token.match(IS_ITALICIZED);
+		if (match?.index != undefined) {
+			let fromOffset = match.index - radius; 
+			let toOffset = match[0].length - ((range.to - range.from) - fromOffset) - 1;
+			// Creates a new selection with `from` and `to` located directly left of the corresponding underscore
+			return EditorSelectionCM.range(range.from + fromOffset, range.to + toOffset);
+		}
+		return range;
+	}
+
+
+	// helper to turn Obsidian line/char range into from/to range for CodeMirror
+	private getFromTo(selection: EditorSelectionOb): EditorRange {
+		let anchorpos = selection.anchor.line + selection.anchor.ch;
+		let headpos = selection.head.line + selection.head.ch;
+		let from = anchorpos < headpos ? selection.anchor : selection.head;
+		let to = anchorpos < headpos ? selection.head : selection.anchor;
+		return { from: from, to: to };
+	}
+
+
 }
 
 class MySettingTab extends PluginSettingTab {
@@ -136,9 +222,9 @@ class MySettingTab extends PluginSettingTab {
 				.addOption('asterisk', 'Asterisk')
 				.setValue(this.plugin.settings.defaultItalic)
 				.onChange(async (value) => {
-					let choice: ItalicCharChoice = value as ItalicCharChoice ?? 'underscore'; // idk???????
+					let choice: 'underscore' | 'asterisk' = (value as 'underscore' | 'asterisk') ?? 'underscore';
 					this.plugin.settings.defaultItalic = choice; 
-					console.log(choice);
+					this.plugin.delim = (choice === 'asterisk') ? '*' : '_';
 					await this.plugin.saveSettings();
 				})
 			);
